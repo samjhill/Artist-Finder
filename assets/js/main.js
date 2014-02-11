6235f394c32fbc90556fe7a8954b2c25baf75a84
Sam Hill <samuhil@gmail.com>
2014-02-11 14:28:43 -0500

    hopefully, fixed scope of ajaxGet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,5 @@
 console.log("main.js loaded");
- var xmlhttp;
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
+ 
 	
 	
 // create a select element with given values.
@@ -25,6 +18,21 @@ function createSelect( values ){
 
 //basic ajax GET call
 function ajaxGet( url ){
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+	    // code for IE7+, Firefox, Chrome, Opera, Safari
+	    xmlhttp = new XMLHttpRequest();
+	} else {
+	    // code for IE6, IE5
+	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	    {
+	    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+	      {
+	      document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+	      }
+	    }
 	xmlhttp.open("GET",url,true);
 	xmlhttp.send();
 	var response = xmlhttp.responseText;
