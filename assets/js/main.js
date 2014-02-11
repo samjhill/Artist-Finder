d79902aa5256d2651c51fa797790910e5798a8d7
Sam Hill <samuhil@gmail.com>
2014-02-11 13:58:23 -0500

    Initial commit.

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+console.log("main.js loaded");
+ var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+	
+	
+// create a select element with given values.
+// values is an array
+function createSelect( values ){
+	var select = document.createElement( 'select' );
+	
+	for( var i = 0; i < values.length; i++ ){
+		var option = document.createElement( 'option' );
+		option.value = values[i];
+		option.text = values[i];
+		select.appendChild( option );
+	}
+	return select;
+}
+
+//basic ajax GET call
+function get( url ){
+	xmlhttp.open("GET",url,true);
+	xmlhttp.send();
+	return xmlhttp.responseText;
+}
+
+//basic ajax POST call
+function post( url, data ){
+	xmlhttp.open("POST",url,true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send( data );
+	return xmlhttp.responseText;
+}
