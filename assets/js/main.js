d648923dd8e89dd35a460e21cf4d5e731e4c882f
Sam J Hill <samuhill@gmail.com>
2014-02-19 11:55:18 -0500

    typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ function get( url ){
 	  var xmlHttp = null;
 
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.open( "GET", url, false );
     xmlHttp.send( null );
     return xmlHttp.responseText;
 	
