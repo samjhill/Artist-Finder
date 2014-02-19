c2af208628a09ee5b431344aed780672bc593dc4
Sam J Hill <samuhill@gmail.com>
2014-02-19 10:42:57 -0500

    fix background tiling of artist image

--- assets/js/main.js
+++ assets/js/main.js
@@ -279,9 +279,8 @@ function getArtistImage( artistName ){
 function setBackground( url ){
 	//document.body.style.backgroundImage = "url('" + url + "') no-repeat center center fixed";
 	console.log('background set to ' + url);
-	document.body.style.backgroundImage = "url('" + url + "')";
-	document.body.style.backgroundSize = "cover";
-	
+	document.body.style.backgroundImage = "url('" + url + "') no-repeat center center fixed";
+	document.body.className += 'bgArtistImage';
 }
 
 function reset(){
